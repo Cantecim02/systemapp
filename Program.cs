d1263805c575baa28e99b7ffe01254bf152ecb06
Cantecim02 <cantecim@outlook.com>
2025-04-29 13:12:25 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+    Giriş:
+        string dogruKullanici = "admin";
+        string dogruSifre = "123456";
+
+        Console.Write("Kullanıcı adınızı girin: ");
+        string kullaniciAdi = Console.ReadLine();
+
+        Console.Write("Şifrenizi girin: ");
+        string sifre = Console.ReadLine();
+
+        if (kullaniciAdi == dogruKullanici && sifre == dogruSifre)
+        {
+            Console.WriteLine("\nGiriş başarılı!");
+        }
+        else
+        {
+            Console.WriteLine("\nLütfen bilgilerinizi kontrol ediniz.\n");
+            goto Giriş;
+        }
+    }
+}
+
